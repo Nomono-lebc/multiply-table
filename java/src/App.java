5bc1932e53d272186925d266268b93c06bb7579e
TheLuisMorsco <luis.bolivar@morsco.com>
2025-05-05 14:06:21 -0600

    added with recursive

--- java/src/App.java
+++ java/src/App.java
@@ -1,11 +1,28 @@
+import java.util.stream.IntStream;
+
 public class App {
     public static void main(String[] args) throws Exception {
         printMultiplyTable(5);
+        System.out.println("Recursive: ");
+        printMultiplyTableRecursive(5, 1);
+        System.out.println("Lambda: ");
+        printMultiplyTableLambda(5);
     }
 
     public static void printMultiplyTable(int n) {
         for (int i = 1; i <= 10; i++) {
             System.out.println(n + " x " + i + " = " + (n * i));
         }
+    }
+
+    public static void printMultiplyTableLambda(final int n) {
+        IntStream.rangeClosed(1, 10)
+        .forEach(i -> System.out.println(n + " x " + i + " = " + (n * i)));
+    }
+
+    public static void printMultiplyTableRecursive(final int n, final int i) {
+        if (i > 10) return;
+        System.out.println(n + " x " + i + " = " + (n * i));
+        printMultiplyTableRecursive(n, i + 1);
     }
 }
--- python/main.py
+++ python/main.py
@@ -2,6 +2,21 @@ def multiplyTable(n):
     for i in range(1, 11):
         print(f"{n} x {i} = {n * i}")
 
+
+def multiplyTableLambda(n):
+    list(map(lambda i: print(f"{n} x {i} = {n * i}"), range(1, 11)))
+
+
+def multiplyTableRecursive(n, i):
+    if i > 10:
+        return
+    print(f"{n} x {i} = {n * i}")
+    multiplyTableRecursive(n, i + 1)
+
 if __name__ == "__main__":
     n = int(input("Enter a number: "))
     multiplyTable(n)
+    print("Recursive:")
+    multiplyTableRecursive(n, 1)
+    print("Lambda:")
+    multiplyTableLambda(n)
